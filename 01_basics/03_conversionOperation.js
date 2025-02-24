cb1801e6510e43ef6093bb662aff21418666077a
Mangeshkanojia <136577607+Mangeshkanojia@users.noreply.github.com>
2025-02-24 04:45:46 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+let score = "33asss"
+
+console.log(typeof score); // typeof
+console.log(typeof(score)); // method
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
