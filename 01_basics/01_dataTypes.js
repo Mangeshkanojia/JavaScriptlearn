de6ba3c74d457483ddf81750d3ef02d902a003ac
Mangeshkanojia <136577607+Mangeshkanojia@users.noreply.github.com>
2025-02-24 04:30:24 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer Vesrion
+
+// alert("hello") // we are use node.js not in browser
+
+console.log(3+3);
+
+let name ="mangesh" // string datatype
+let age = 18 // int
+let isLoggoedIn = false // boolean
+
+// number => 2 to pwoer 53
+// brigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>  value
