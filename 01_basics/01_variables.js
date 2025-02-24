55e82b0b56d61f0d2bda2fbd6ba262d7fb287f79
Mangeshkanojia <136577607+Mangeshkanojia@users.noreply.github.com>
2025-02-24 04:07:51 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 123456
+let accountEmail ="mangeshkanojia580@gmail.com"
+var accountPassword = 123456
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
