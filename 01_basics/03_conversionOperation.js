0379a5da96db06cd25a3ac36a0985c9783da00b0
Mangeshkanojia <136577607+Mangeshkanojia@users.noreply.github.com>
2025-02-25 04:21:22 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,23 @@
 let score = "33asss"
 
-console.log(typeof score); // typeof
-console.log(typeof(score)); // method
+// console.log(typeof score); // typeof
+// console.log(typeof(score)); // method
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+
+let a = 5;
+let b = 5;
+let c = a+b;
+let d = a-b;
+
+console.log(c);
+console.log(d);
 
